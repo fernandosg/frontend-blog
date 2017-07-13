bb749c72d9c9c56d94b4115f94b96b8b5680057a
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 00:58:43 -0500

    Combining categoryReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import postReducer from './postReducer';
-
+import categoryReducer from './categoryReducer';
 const rootReducer=combineReducers({
-  postReducer:postReducer
+  postReducer:postReducer,
+  categoryReducer:categoryReducer
 });
 
 export default rootReducer;
