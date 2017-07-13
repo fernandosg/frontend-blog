74b4e36ef4d31a0bf8fe08a70dcf462106da6e34
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 01:32:09 -0500

    Implemented getting all the categories for showing when someone visit the list of categories.

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,12 +1,35 @@
 import{
   GET_BY_ID,GET_ALL
 } from '../actions/category/types';
+import {
+  SPACE_ID,ACCESS_TOKEN
+} from '../config.js';
 
+const contentful = require('contentful')
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: ACCESS_TOKEN
+})
 export default function(state=[],action){
   switch (action.type) {
     case GET_ALL:
+      getAllCategories();
       return state;
     default:
       return state;
   }
+}
+
+function getAllCategories(){
+  client.getEntries({
+    content_type:"category"
+  }).then((response)=>{
+    console.dir(response.items);
+    response.items;
+  }).catch((error)=>{
+    console.log("Hubo un error =/");
+    console.dir(error);
+  })
 }
