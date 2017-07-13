8fd868ae49154e5887ff9b287ad1f274e3e872dd
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 00:43:41 -0500

    Adding components for UI, and a blank reducer index.

---
+++ src/components/category.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Category extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Testing category</h1>
+      </div>
+    )
+  }
+}
+
+export default Category;
---
+++ src/components/post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Post extends Component{
+  render(){
+    return(
+      <article>
+        <h1>Titulo del post</h1>
+        <p>Mensaje</p>
+        <b>13/07/2017</b>
+      </article>
+    )
+  }
+}
+
+export default Post;
