77cef9d2b11f6c208070c98ed0ed803f69536329
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 00:43:53 -0500

    Adding routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Category from './components/category';
+import Post from './components/post';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import {Router, Route, browserHistory} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/category/:id" component={Category}/>
+      <Route path="/post/:id" component={Post}/>
+    </Route>
+  </Router>
+  , document.getElementById('root'));
 registerServiceWorker();
