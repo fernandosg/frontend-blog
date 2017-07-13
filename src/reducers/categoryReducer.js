9de25b35e68362b55f72c5aa2d632f0413078a05
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 02:36:56 -0500

    Refactoring category reducer

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,35 +1,16 @@
 import{
   GET_BY_ID,GET_ALL
 } from '../actions/category/types';
-import {
-  SPACE_ID,ACCESS_TOKEN
-} from '../config.js';
 
-const contentful = require('contentful')
-const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: SPACE_ID,
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: ACCESS_TOKEN
-})
 export default function(state=[],action){
   switch (action.type) {
-    case GET_ALL:
-      getAllCategories();
-      return state;
+    case `${GET_ALL}_PENDING`:
+      return [];
+    case `${GET_ALL}_FULFILLED`:
+      return [...state,...action.payload];
+    case `${GET_ALL}_REJECTED`:
+      return [];
     default:
       return state;
   }
-}
-
-function getAllCategories(){
-  client.getEntries({
-    content_type:"category"
-  }).then((response)=>{
-    console.dir(response.items);
-    response.items;
-  }).catch((error)=>{
-    console.log("Hubo un error =/");
-    console.dir(error);
-  })
 }
