5c864c6ce3b6be8d96a35e226a004e1a1926f98c
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 01:25:53 -0500

    Added new component for handle a list of categories.

---
+++ src/components/list_category.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListCategory extends Component{
+  render(){
+    return(
+      <h1>Lista de categorias</h1>
+    )
+  }
+}
+
+export default ListCategory;
