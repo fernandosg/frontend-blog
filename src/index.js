0b74a6c65986705e89b7cd318ca13b2f7285e7ab
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 00:55:18 -0500

    Using redux with the combination of reducers.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import Category from './components/category';
 import Post from './components/post';
 import registerServiceWorker from './registerServiceWorker';
 import {createStore, applyMiddleware} from 'redux';
+import reducers from './reducers';
 import {Router, Route, browserHistory} from 'react-router';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="/category/:id" component={Category}/>
-      <Route path="/post/:id" component={Post}/>
-    </Route>
-  </Router>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/category/:id" component={Category}/>
+        <Route path="/post/:id" component={Post}/>
+      </Route>
+    </Router>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
