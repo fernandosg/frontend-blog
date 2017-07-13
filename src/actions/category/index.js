53da5a1af4e4a3f68c58a966a02f86fcec22ade0
fernandosg <fsg_tmp@hotmail.com>
2017-07-13 02:37:39 -0500

    Refactoring code, and passed all the process for reques to the action creator.

--- src/actions/category/index.js
+++ src/actions/category/index.js
@@ -1,9 +1,35 @@
 import{
   GET_ALL
 } from './types';
+
+
+import {
+  SPACE_ID,ACCESS_TOKEN
+} from '../../config.js';
+
+const contentful = require('contentful')
+
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: ACCESS_TOKEN
+})
 export function getAllCategories(){
+  const request=client.getEntries({
+    content_type:"category"
+  }).then((response)=>{
+    return response.items.map((category)=>{
+      return {
+        name:category.fields.name,
+        id:category.sys.id
+      }
+    });
+  }).catch((error)=>{
+    console.log("Hubo un error");
+  })
   return {
     type: GET_ALL,
-    payload: 0
+    payload: request
   }
 }
