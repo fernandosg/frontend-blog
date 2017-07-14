08ffcb1b14d3199083d2f2d96c09c04531e791ca
fernandosg <fsg_tmp@hotmail.com>
2017-07-14 17:02:27 -0500

    Refactoring code.

--- src/actions/post/index.js
+++ src/actions/post/index.js
@@ -13,11 +13,12 @@ const client = contentful.createClient({
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: ACCESS_TOKEN
 })
-export function getAllPostByCategory(category){
+export function getAllPostByCategory(category_name){
   const request=client.getEntries({
     access_token:ACCESS_TOKEN,
     content_type:"post",
-    "fields.category.sys.contentType.sys.id":category
+    "fields.category.sys.contentType.sys.id":"category",
+    "fields.category.fields.name[match]":category_name
   }).then((response)=>{
     return response.items.map((post)=>{
       return{
