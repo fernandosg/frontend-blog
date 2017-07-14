ec0782fc59ff3d232e2ff1f8463ae27e167eea69
fernandosg <fsg_tmp@hotmail.com>
2017-07-14 13:22:18 -0500

    Added  a new method in action creator for handle the request of an individual post.

--- src/actions/post/index.js
+++ src/actions/post/index.js
@@ -1,5 +1,5 @@
 import{
-  GET_BY_CATEGORY
+  GET_BY_CATEGORY, GET_BY_ID
 } from './types';
 
 import {
@@ -34,6 +34,29 @@ export function getAllPostByCategory(category){
   });
   return{
     type: GET_BY_CATEGORY,
+    payload: request
+  }
+}
+
+export function getPostById(post_id){
+  const request=client.getEntries({
+    access_token:ACCESS_TOKEN,
+    content_type:"post",
+    "fields.contentType.sys.Id":post_id
+  }).then((response)=>{
+    return{
+      id:response.items[0].sys.id,
+      title:response.items[0].fields.title,
+      message:response.items[0].fields.message,
+      resume_message:response.items[0].fields.resumeMessage,
+      published_at:response.items[0].fields.publishedAt,
+      category_name:response.items[0].fields.category.fields.name
+    }
+  }).catch((error)=>{
+    console.log("Hubo un error buscando post por categoria")
+  });
+  return{
+    type: GET_BY_ID,
     payload: request
   }
 }
