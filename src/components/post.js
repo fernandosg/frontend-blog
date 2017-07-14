df3b1ca8cfeb58544dd91ab10ffee12d7ebbb505
fsgom <fernandosegom@gmail.com>
2017-07-14 13:21:49 -0500

    Refactoring code. Now when a Post component is mounted, is detecting if the mounting process is on a list of post or on a individual post, this is because, if is a individual post, a next request is made by getting the information of the post.

--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actions from '../actions/post';
+import { Link } from 'react-router';
 class Post extends Component{
 
   constructor(){
     super();
     this.handleContentOfPost=this.handleContentOfPost.bind(this);
+    this.getLayoutPost=this.getLayoutPost.bind(this);
+  }
+
+  componentWillMount(){
+    if(this.props.preview==undefined)
+      this.props.getPostById(this.props.id);
   }
 
   handleContentOfPost(){
@@ -19,15 +27,40 @@ class Post extends Component{
     }
   }
 
+  getLayoutPost(){
+    if(this.props.preview==true){
+      let path="/post/"+this.props.id;
+      return <article>
+        <h1>
+          <Link to={path}>{this.props.title}</Link>
+        </h1>
+        <p>{this.props.resume_message}</p>
+        <b>{this.props.published_at}</b>
+      </article>
+    }else if(this.props.posts.length>0){
+      return <article>
+        <h1>{this.props.posts[0].title}</h1>
+        <p>{this.props.posts[0].message}</p>
+        <b>{this.props.posts[0].published_at}</b>
+      </article>
+    }else{
+      <article></article>
+    }
+  }
+
   render(){
     return(
       <article>
-        <h1>{this.props.title}</h1>
-        {this.handleContentOfPost()}
-        <b>{this.props.published_at}</b>
+        {this.getLayoutPost()}
       </article>
     )
   }
 }
 
-export default Post;
+function mapStateToProps(state){
+  return{
+    posts:state.postReducer
+  }
+}
+
+export default connect(mapStateToProps,actions)(Post);
