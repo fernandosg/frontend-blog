e07c216705c5556b2086cff570c1d32ee18086b4
fernandosg <fsg_tmp@hotmail.com>
2017-07-14 17:11:15 -0500

    Added marginBottom in the header (just for test)

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import ListCategory from './list_category';
 class Header extends Component{
   render(){
     return(
-      <div className="App-header">
+      <div className="App-header" style={{marginBottom:10}}>
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
         <ListCategory />
