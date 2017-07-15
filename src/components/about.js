afc6c6d5ac501bb42a0179bfe1e395bf7944994f
fernandosg <fsg_tmp@hotmail.com>
2017-07-15 15:29:44 -0500

    Added some style in about component.

--- src/components/about.js
+++ src/components/about.js
@@ -3,24 +3,28 @@ import React, { Component } from 'react';
 class About extends Component{
   render(){
     return(
-      <div>
+      <div className="container-about">
         <h1>Acerca de mí y este blog</h1>
         <p>Mi nombre es Fernando Segura, soy programador freelance para web (principalmente backend) y móvil en plataformas android.</p>
         <p>Lo se, otro blog de un programador con su nombre en el dominio ¿que se pudiera encontrar en otro blog de este tipo?.</p>
         <p>En particular este blog lo hice por dos razones:</p>
-        <p>1.- Casi todos los días programo algo, ya sea un ejercicio, o por alguna razón se me ocurre un “emprendimiento” (realmente son sistemas relativamente sencillos bajo un contexto altruista xD). </p>
+        <p><b>1.-</b> Casi todos los días programo algo, ya sea un ejercicio, o por alguna razón se me ocurre un “emprendimiento” (realmente son sistemas relativamente sencillos bajo un contexto altruista xD). </p>
         <p>En el proceso me encuentro con problemas técnicos con librerías o API’s, y en ocasiones doy soluciones entre una mezcla de posts en stackoverflow, issues reportados más no solucionados en Github, y una indagación por la documentación oficial. </p>
-        <p>Doy con la solución, la aplico, pero en ocasiones se vuelve repetitivo en otros proyectos, y llegó a olvidar un poco del porque la solución.</p>
-    Voy a estar compartiendo en este blog en cuanto encuentre una problemática de este tipo, para aquel que llegue a toparse con este sitio, o por buena práctica y tener una buena comprensión de la solución.
-      <p>2.- Práctica. En el punto anterior mencione que casi todos los días programo algo, en particular en la semana donde escribo esto (10 de julio-16 de julio del 2017) me encontré la idea de hacer un mini proyecto en 1 día. Este blog fue concebido para practicar un poco React+Redux, aunque fracase en mi intento de hacerlo en 1 día (tuve una semana algo movida y no pude dedicarme en mis horas libres para terminarlo).</p>
-    
+        <p>Doy con la solución, la aplico, pero en ocasiones se vuelve repetitivo en otros proyectos, y llegó a olvidar un poco del porque la solución.
+        Voy a estar compartiendo en este blog en cuanto encuentre una problemática de este tipo, para aquel que llegue a toparse con este sitio, o por buena práctica y tener una buena comprensión de la solución.</p>
+      <p><b>2.-</b> Práctica. En el punto anterior mencione que casi todos los días programo algo, en particular en la semana donde escribo esto (10 de julio-16 de julio del 2017) me encontré la idea de hacer un mini proyecto en 1 día. Este blog fue concebido para practicar un poco React+Redux, aunque fracase en mi intento de hacerlo en 1 día (tuve una semana algo movida y no pude dedicarme en mis horas libres para terminarlo).</p>
+
     <h2>¿Qué contenido compartiré?.</h2>
     <p>En particular me gusta programar con Ruby (Rails), Javascript (React+Redux, JQuery, me interesa React Native pero no me he metido a investigar, me interesa la Realidad Aumentada), Android. Desde hace unos meses me interesa aplicar realmente TDD y BDD a mi flujo de trabajo, estarás leyendo muy probablemente sobre esto en el blog.</p>
     <p>Soy fan de asistir a eventos de emprendimiento, en particular los Startup Weekend.</p>
-    <p>¿Eres programador? Bienvenido, sientete libre de si ves algo técnico y deseas hacer una crítica de algo hacerla.</p>
-    <p>¿Eres emprendedor? Bienvenido, en particular me llama la atención todo lo relacionado con el emprendimiento social/altruista, quizás encuentres alguna nota donde puedas comentar algo al respecto. De igual manera, si tienes una idea de proyecto, y buscas alguien que te apoye, contactame y platicamos, quizás podamos lograr algo juntos: fernandosegom@gmail.com</p>
-    <p>¿No tienes idea de como llegaste aqui?: Suele suceder en ocasiones, siéntete libre de dejar cualquier comentario que gustes.</p>
+    <p><b>¿Eres programador?</b> Bienvenido, sientete libre de si ves algo técnico y deseas hacer una crítica de algo hacerla.</p>
+    <p><b>¿Eres emprendedor?</b> Bienvenido, en particular me llama la atención todo lo relacionado con el emprendimiento social/altruista, quizás encuentres alguna nota donde puedas comentar algo al respecto. De igual manera, si tienes una idea de proyecto, y buscas alguien que te apoye, contactame y platicamos, quizás podamos lograr algo juntos: fernandosegom@gmail.com</p>
+    <p><b>¿No tienes idea de como llegaste aqui?</b>: Suele suceder en ocasiones, siéntete libre de dejar cualquier comentario que gustes.</p>
     <p>Saludos, bienvenido a este blog :), ten un excelente día.</p>
+    <p>Puedes saber un poco mas de mi desde mis otras redes sociales:</p>
+    <p><a href="https://www.linkedin.com/in/fernando-segura-72139928/">Linkedin</a>.</p>
+    <p><a href="https://twitter.com/fernandosegom">Twitter</a></p>
+
       </div>
 
 
