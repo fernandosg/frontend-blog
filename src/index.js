e7c1b288d6c502609abea3b6e9c03a083246078a
fernandosg <fsg_tmp@hotmail.com>
2017-07-14 20:20:35 -0500

    Added new component for default route.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import Principal from './components/principal';
 import Category from './components/category';
 import ListCategory from './components/list_category';
 import Post from './components/post';
@@ -10,13 +11,14 @@ import About from './components/about';
 import registerServiceWorker from './registerServiceWorker';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import promiseMiddleware from 'redux-promise-middleware';
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware())(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Principal}/>
         <Route path="/category" component={ListCategory}/> //This is for test, the ListCategory maybe should be in the header or in the sidebar
         <Route path="/category/:category_name" component={Category}/>
         <Route path="/post/:id" component={Post}/>
