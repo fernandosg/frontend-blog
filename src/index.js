0e7a0306994e07b0ecc184f580a8f691298e227f
fernandosg <fsg_tmp@hotmail.com>
2017-07-15 14:27:41 -0500

    Removed unnecessary code.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import Principal from './components/principal';
 import Category from './components/category';
-import ListCategory from './components/list_category';
 import Post from './components/post';
 import About from './components/about';
 import registerServiceWorker from './registerServiceWorker';
@@ -19,7 +18,6 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Principal}/>
-        <Route path="/category" component={ListCategory}/> //This is for test, the ListCategory maybe should be in the header or in the sidebar
         <Route path="/category/:category_name" component={Category}/>
         <Route path="/post/:id" component={Post}/>
         <Route path="/about" component={About}/>
