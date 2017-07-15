39cbd18ef49915f6f8a02396dd620b58e112d6d0
fernandosg <fsg_tmp@hotmail.com>
2017-07-14 20:20:06 -0500

    Using Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header';
+import Footer from './components/footer';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         {this.props.children}
+        <Footer />
       </div>
     );
   }
