444fab5fe2044ed11ec6c1374ee9fdaba7103dde
fernandosg <fsg_tmp@hotmail.com>
2017-07-15 01:06:45 -0500

    Added the new principalReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import postReducer from './postReducer';
 import categoryReducer from './categoryReducer';
+import principalReducer from './principalReducer';
 const rootReducer=combineReducers({
   postReducer:postReducer,
-  categoryReducer:categoryReducer
+  categoryReducer:categoryReducer,
+  principalReducer:principalReducer
 });
 
 export default rootReducer;
